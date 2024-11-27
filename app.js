accc8f1ce7a3d3aae1ccd9c3f0d3796b2c1b2510
Zsotl <b0papzso@vasvari.org>
2024-11-27 14:27:18 +0100

    3. feladat

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 import express from 'express'
-import routes from './routes/routes.js'
+import defaultroute from './routes/defaultroutes.js'
 import expressroute from './routes/expressroutes.js'
 import nodejsroute from './routes/nodejsroutes.js'
 import greeting from './routes/greetingroutes.js'
+import bodyParser from 'body-parser'
 
 const app = express()
 const PORT = 3000
 
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use("/", defaultroute)
 app.use('/greetings', greeting)
 app.use('/express', expressroute)
 app.use('/nodejs', nodejsroute)
--- util/rootpath.js
+++ util/rootpath.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const rootpath = path.join(__dirname, '..')
+const rootpath = path.join(__dirname, '')
 console.log(rootpath)
 
 export default rootpath
